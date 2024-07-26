3fe58b853b9e682eefde076f1a78cb1ee5f52a12
Raúl Corchero <corchero100@gmail.com>
2024-07-26 23:37:57 +0100

    Implements: AddFlagUseCase

---
+++ src/FeatureFlags.Web/Controllers/FlagsController.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Claims;
+using FeatureFlags.Web.UseCase;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FeatureFlags.Web.Controllers {
+    [Authorize]
+    [Route("[controller]")]
+    public class FlagsController : Controller
+    {
+        private readonly IAddFlagUseCase _addFlagUseCase;
+
+        public FlagsController(IAddFlagUseCase addFlagUseCase)
+        {
+            _addFlagUseCase = addFlagUseCase;
+        }
+
+        [HttpGet("create")]
+        public IActionResult Create()
+        {
+            return View(new FlagViewModel());
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> AddFlagToDatabase(FlagViewModel request)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            bool isCreated = await _addFlagUseCase.Execute(request.Name, request.IsEnabled);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+    public class FlagViewModel
+    {
+        public string Name { get; set; }
+        public bool IsEnabled { get; set; }
+    }
+}
---
+++ src/FeatureFlags.Web/UseCase/AddFlagUseCase.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Claims;
+using FeatureFlags.Web.Data;
+using FeatureFlags.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FeatureFlags.Web.UseCase {
+    public class AddFlagUseCase : IAddFlagUseCase
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public AddFlagUseCase(ApplicationDbContext applicationDbContext, IHttpContextAccessor httpContextAccessor)
+        {
+            _applicationDbContext = applicationDbContext;
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public async Task<bool> Execute(string flagName, bool isActive)
+        {
+            string userId = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            FlagEntity entity = new()
+            {
+                Name = flagName,
+                UserId = userId,
+                Value = isActive
+            };
+            var response = await _applicationDbContext.Flags.AddAsync(entity);
+            await _applicationDbContext.SaveChangesAsync();
+            return true;
+        }
+    }
+
+    public interface IAddFlagUseCase
+    {
+        Task<bool> Execute(string flagName, bool isActive);
+    }
+}
